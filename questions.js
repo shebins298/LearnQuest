4acbccb4d559246c450fa9144e28e24f1e7cfca8
shebins298 <shebins298@gmail.com>
2025-06-11 10:38:57 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,84 @@
+const cppQuestions = {
+  "Chapter 1 - Review of C++ Programming": [
+    {
+      question: "The input operator in C++ is _________",
+      options: ["<<", ">>", "cin", "get"],
+      answer: ">>",
+    },
+    {
+      question: "Which operator is an arithmetic assignment operator in C++?",
+      options: ["==", "+=", "=", "++"],
+      answer: "+=",
+    },
+    {
+      question:
+        "Which among the following is equivalent to the statement series b=a; a=a+1; ?",
+      options: ["b += a", "b = a++", "b = ++a", "b += a + b"],
+      answer: "b = a++",
+    },
+
+    {
+      question:
+        "A __________ statement in a loop forces the termination of that loop.",
+      options: ["continue", "exit", "return", "break"],
+      answer: "break",
+    },
+    {
+      question: "Which of the following is an exit control loop in C++?",
+      options: ["while", "for", "do while", "if"],
+      answer: "do while",
+    },
+    {
+      question: "Which among the following is an insertion operator in C++?",
+      options: ["<<", ">>", "cin", "get"],
+      answer: "<<",
+    },
+
+    {
+      question: "Which among the following is not a valid identifier in C++?",
+      options: ["total_sum", "123value", "_count", "marks"],
+      answer: "123value",
+    },
+    {
+      question: "Which data type is best suited to store a single character?",
+      options: ["int", "char", "string", "float"],
+      answer: "char",
+    },
+
+    {
+      question:
+        "What will be the value of x after executing: int x = 10; x += 5;",
+      options: ["5", "10", "15", "20"],
+      answer: "15",
+    },
+    {
+      question:
+        "Which of the following is not a valid data type modifier in C++?",
+      options: ["signed", "unsigned", "long", "fixed"],
+      answer: "fixed",
+    },
+  ],
+
+  Tokens: [
+    {
+      question: "Which of the following is not a C++ token?",
+      options: ["Keyword", "Identifier", "Pointer", "Operator"],
+      answer: "Pointer",
+    },
+    {
+      question: "Which symbol is used to denote a preprocessor directive?",
+      options: ["@", "#", "$", "&"],
+      answer: "#",
+    },
+    {
+      question: "Which of the following is a valid identifier in C++?",
+      options: ["123abc", "_abc", "int", "if"],
+      answer: "_abc",
+    },
+    {
+      question: "Which of these is an example of a keyword in C++?",
+      options: ["class", "main", "printf", "scanf"],
+      answer: "class",
+    },
+  ],
+};
